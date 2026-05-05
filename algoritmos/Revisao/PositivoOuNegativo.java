5fc83f0bbc26577cdce178625693414084dc05a5
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-04 21:00:47 -0300

    Update PositivoOuNegativo.java

--- algoritmos/Revisao/PositivoOuNegativo.java
+++ algoritmos/Revisao/PositivoOuNegativo.java
@@ -4,15 +4,15 @@ public class PositivoOuNegativo {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
         int numero;
+        String result;
         
         System.out.println("Digite um número inteiro: ");
         numero = ent.nextInt();
         
-        if(numero < 0){
-            System.out.println("O número "+numero+" é negativo");
-        } else {
-            System.out.println("O número "+numero+" é positivo");
-        }
+        result = (numero < 0)? "é negativo": "é positivo";
+        
+        System.out.println("O número "+numero+" "+result);
+        ent.close();
     }
     
 }
