6d59d601053e0f843a9baa2c3f6771418212d1d1
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-04 21:10:02 -0300

    Update SomaDosNumeros.java

--- algoritmos/Revisao/SomaDosNumeros.java
+++ algoritmos/Revisao/SomaDosNumeros.java
@@ -5,6 +5,7 @@ public class SomaDosNumeros {
         Scanner ent = new Scanner(System.in);
         int numero;
         int soma = 0;
+        int quantNumeros = 0;
         
         System.out.println("Digite 0 a qualquer momemento para somar");
         do {
@@ -12,9 +13,14 @@ public class SomaDosNumeros {
             numero = ent.nextInt();
             
             soma += numero;
+            if (numero != 0){
+               quantNumeros++;
+            }
+           
         }while(numero != 0);
         
         System.out.println("A soma dos números digitados é "+soma);
+        System.out.println("A quantidade de números digitados foram "+quantNumeros);
         ent.close();
     }
     
