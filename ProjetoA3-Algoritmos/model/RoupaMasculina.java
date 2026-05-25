85604db690c67ce2b3685f220277affc313e5794
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-25 19:01:32 -0300

    Create RoupaMasculina.java

---
+++ ProjetoA3-Algoritmos/model/RoupaMasculina.java
@@ -0,0 +1,9 @@
+// --- MASCULINA ---
+class RoupaMasculina extends Roupa {
+    private String tipoCorte;
+    public RoupaMasculina(String nome, String marca, String tamanho, double preco, String cor, String tipoCorte) {
+        super(nome, marca, tamanho, preco, cor);
+        this.tipoCorte = tipoCorte;
+    }
+    @Override public String toString() { return super.toString() + " | Corte: " + tipoCorte; }
+}
