4c16fb8f19861507da3a34e3debae9b87327f83c
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-25 18:59:42 -0300

    Create CalcadoMenino.java

---
+++ ProjetoA3-Algoritmos/model/CalcadoMenino.java
@@ -0,0 +1,9 @@
+// --- MENINO ---
+class CalcadoMenino extends Calcado {
+    private String temaPersonagem;
+    public CalcadoMenino(String nome, String marca, int numeracao, double preco, String cor, String temaPersonagem) {
+        super(nome, marca, numeracao, preco, cor);
+        this.temaPersonagem = temaPersonagem;
+    }
+    @Override public String toString() { return super.toString() + " | Tema: " + temaPersonagem; }
+}
