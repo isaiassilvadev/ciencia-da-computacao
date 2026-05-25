18a64c2936fa472b34898cb9e3642c332bab6517
Isaias Silva <isaias.silva.dev@gmail.com>
2026-05-25 18:58:16 -0300

    Update CalcadoMasculino.java

--- ProjetoA3-Algoritmos/model/CalcadoMasculino.java
+++ ProjetoA3-Algoritmos/model/CalcadoMasculino.java
@@ -10,16 +10,6 @@ class CalcadoMasculino extends Calcado {
     @Override public String toString() { return super.toString() + " | Estilo: " + tipoUso; }
 }
 
-// --- FEMININO ---
-class CalcadoFeminino extends Calcado {
-    private String tipoSalto;
-    public CalcadoFeminino(String nome, String marca, int numeracao, double preco, String cor, String tipoSalto) {
-        super(nome, marca, numeracao, preco, cor);
-        this.tipoSalto = tipoSalto;
-    }
-    @Override public String toString() { return super.toString() + " | Salto: " + tipoSalto; }
-}
-
 // --- MENINO ---
 class CalcadoMenino extends Calcado {
     private String temaPersonagem;
